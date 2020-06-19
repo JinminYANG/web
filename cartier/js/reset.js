2b67bf45e7be56111e08fd4d40a525290c13600c
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-06-19 18:14:49 +0900

    web_5

---
+++ cartier/js/reset.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// alert('sfs');
+
+	// $('section.contents>article').on("mouseover", function(){
+	// 	$('section.contents').css("width","70%");
+	// 	$(this).css("width","60%");
+	// 	$(this).siblings().css("width","13.3%");
+	// 	var i = this.index();
+	// 	$('section.contents>article').siblings().eq(i).css("opacity","1");
+	// });
+
+	// 마우스 오버
+
+
+
+
+	// 마우스 아웃
+});	
