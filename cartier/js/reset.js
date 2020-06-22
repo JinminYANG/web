0033f6bc7f4728310483aa31d5abb11cf1100de1
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-06-22 18:17:27 +0900

    web_5

--- cartier/js/reset.js
+++ cartier/js/reset.js
@@ -10,9 +10,27 @@ $(document).ready(function(){
 	// });
 
 	// 마우스 오버
-
+	$('article').on('mouseover',function(){
+		var vid = $(this).find("video").get(0);
+		vid.currentTime=0; //동영상의 재생위치를 처음(0)으로 설정
+		vid.play();
+		$(this).stop().animate({'width':'35%'},1000,function(){
+			$(this).find('h3').stop().animate({"right":"10px"},400);
+			$(this).find('p').stop().animate({"right":"10px"},800);
+		});
+		$(this).find("video").stop().animate({"opacity":"0.9"},1200);
+	});
 
 
 
 	// 마우스 아웃
+	$('article').on('mouseout',function(){
+		var vid = $(this).find("video").get(0);
+		vid.pause();
+		$(this).stop().animate({'width':'12%'},500,function(){
+			$(this).find('h3').stop().animate({"right":"-310px"},500);
+			$(this).find('p').stop().animate({"right":"-310px"},500);
+		});
+		$(this).find("video").stop().animate({"opacity":"0"},500);
+	});
 });	
