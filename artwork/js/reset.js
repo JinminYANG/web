f13ddb08e9f7a51166c2d65453ca696c536200ce
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-06-24 18:17:12 +0900

    web_7

---
+++ artwork/js/reset.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	// alert('hi');
+
+	// $('article>h2>a').on().click(function(){
+	// 	var i = $(this).getElementById().eq(0);
+	// 	$('article>p>img').src(i);
+	// });
+
+	$('#navi>li>a').on("click", function(){
+		$('section>article').css("left","0px");
+	});
+});
