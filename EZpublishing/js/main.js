17c3ed8d645cd7ace10b1c009fc198754e13d325
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-07-14 18:16:14 +0900

    web

---
+++ EZpublishing/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$('#container').addClass('start');
+	
+	$('nav li').click(function(){
+		$('#container').css('max-width','100%');
+		var id=$(this).attr('data-rol');
+		$('nav li').removeClass('on');
+		$(this).addClass('on');
+		$('.content').removeClass('prev next this');
+		$('#'+id).addClass('this').prevAll().addClass('prev');
+		$('#'+id).nextAll().addClass('next');
+	});
+
+	// 로고 클릭 시
+	$('.logo_box').click(function(){
+		$('nav li').removeClass('on');
+		$('.content').removeClass('prev next this');
+		$('#container').css('max-width','1200px');
+	});
+
+	// 롤링배너 왼쪽
+	$(".roll_left").on("click", function(){
+		$(".book_roll li").eq(0).insertAfter(".book_roll li:last-child");
+	});
+
+	// 롤링배너 오른쪽
+	$(".roll_right").on("click", function(){
+		$(".book_roll li:last-child").insertBefore(".book_roll li:first-child");
+	});
+});
