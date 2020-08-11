78fba5bda435cfa997d888c3a022161a1f489ac2
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-08-11 18:18:25 +0900

    web

---
+++ team_project-v2/js/login_practice.js
@@ -0,0 +1,27 @@
+$(function() {
+    var $frm = $("#loginForm");
+    $frm.on("submit", function(e) {
+        e.preventDefault();
+        var myData = $frm.serialize();
+        $.ajax({
+            type: "post",
+            url: $frm.attr("action"),
+            data: myData,
+            success: function(res) {
+                if (res) {
+                    alert("로그인 되었습니다.");
+                    var jsonData = JSON.parse(res);
+                    var message = jsonData.user_name + "(" + jsonData.user_id + ")" + "님 반갑습니다.";
+                    $(".tnb").html(message);
+                    $(".cont__login").html(message);
+//                    console.log("로그인 되었습니다.");
+//                    var childwin = window.open("index.html", "_blank"),
+//                    var txt = childwin.getElementsByClassName("tnb");
+//                    txt.html(message);
+                }else{
+                    alert("다시 입력해주세요.");
+                }
+            }
+        });
+    });
+});
