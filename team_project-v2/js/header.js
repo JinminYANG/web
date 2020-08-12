7548f75dc0ee83f10e151f95e2bdb77e51b2bfee
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-08-12 18:17:24 +0900

    web

--- team_project-v2/js/header.js
+++ team_project-v2/js/header.js
@@ -1,41 +1,54 @@
 "use strict";
-var hd=document.querySelector("header"),
-gnb=hd.querySelector(".gnb"),
-hover=gnb.querySelectorAll(".hover"),
-hoverA=gnb.querySelectorAll(".hover a"),
-ul=gnb.querySelectorAll(".hover > ul");
-function none(){
-	hd.classList.remove("show"),
-	ul.forEach(function(e){
-		e.style.height=0,
-		e.style.marginTop=0,
-		e.style.opacity=0,
-		e.style.visibility="hidden"
-	})
+var hd = document.querySelector("header"),
+    gnb = hd.querySelector(".gnb"),
+    hover = gnb.querySelectorAll(".hover"),
+    hoverA = gnb.querySelectorAll(".hover a"),
+    ul = gnb.querySelectorAll(".hover > ul");
+
+function none() {
+    hd.classList.remove("show"),
+        ul.forEach(function (e) {
+            e.style.height = 0,
+                e.style.marginTop = 0,
+                e.style.opacity = 0,
+                e.style.visibility = "hidden"
+        })
 }
-function show(){
-	hd.classList.add("show"),
-	ul.forEach(function(e){
-		e.style.height="200px",
-		e.style.marginTop="20px",
-		e.style.opacity=1,e.style.visibility="visible"
-	})
+
+function show() {
+    hd.classList.add("show"),
+        ul.forEach(function (e) {
+            e.style.height = "200px",
+                e.style.marginTop = "20px",
+                e.style.opacity = 1, e.style.visibility = "visible"
+        })
 }
-function init(){
-	hover.forEach(function(e){
-		e.addEventListener("mouseover",show),
-		e.addEventListener("mouseleave",none)
-	}),
-	hoverA.forEach(function(e){
-		e.addEventListener("focus",show),
-		e.addEventListener("blur",none)
-	})
+
+function init() {
+    hover.forEach(function (e) {
+            e.addEventListener("mouseover", show),
+                e.addEventListener("mouseleave", none)
+        }),
+        hoverA.forEach(function (e) {
+            e.addEventListener("focus", show),
+                e.addEventListener("blur", none)
+        })
 }
 
 init();
 
-//$(function(){
-//    $(".tnb").load("login.html .tnb");
-//    $(".tnb").html(log);
-//    alert(log);
-//});
+
+var newWindow;
+
+function openNewWindow() {
+    newWindow = window.open("login.html", "newWindow");
+}
+
+function sendMeData(data) {
+    $(".tnb").html(data);
+//    var txt = "<font color='red'>자식창에서 받아온 값 : </font>";
+//    document.getElementById("process").innerHTML = txt;
+//    document.getElementById("result").innerHTML = data;
+//    document.getElementsByClassName(".tnb").innerHTML = data;
+    console.log(data);
+}
--- team_project-v2/js/login_practice.js
+++ team_project-v2/js/login_practice.js
@@ -1,27 +1,33 @@
-$(function() {
+$(function () {
     var $frm = $("#loginForm");
-    $frm.on("submit", function(e) {
+    $frm.on("submit", function (e) {
         e.preventDefault();
         var myData = $frm.serialize();
         $.ajax({
             type: "post",
             url: $frm.attr("action"),
             data: myData,
-            success: function(res) {
+            success: function (res) {
                 if (res) {
                     alert("로그인 되었습니다.");
                     var jsonData = JSON.parse(res);
                     var message = jsonData.user_name + "(" + jsonData.user_id + ")" + "님 반갑습니다.";
                     $(".tnb").html(message);
                     $(".cont__login").html(message);
-//                    console.log("로그인 되었습니다.");
-//                    var childwin = window.open("index.html", "_blank"),
-//                    var txt = childwin.getElementsByClassName("tnb");
-//                    txt.html(message);
-                }else{
+                    //                    console.log("로그인 되었습니다.");
+                    //                    var childwin = window.open("index.html", "_blank"),
+                    //                    var txt = childwin.getElementsByClassName("tnb");
+                    //                    txt.html(message);
+                } else {
                     alert("다시 입력해주세요.");
                 }
             }
         });
     });
 });
+
+function send() {
+    var data = document.loginForm.user_id.value;
+    window.opener.sendMeData(data + "님 환영합니다.");
+//    window.close();
+}
