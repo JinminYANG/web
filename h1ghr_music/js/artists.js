f3ec4ccc4ee3dfeaf269e834c2355ebfd8f6fb1a
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-08-19 18:15:05 +0900

    web

--- h1ghr_music/js/artists.js
+++ h1ghr_music/js/artists.js
@@ -2,11 +2,11 @@ function sub(){
 	var sub = document.getElementsByClassName("artists_view"),
 	sc_wid = screen.availWidth;
 	sc_hei = screen.availHeight;
-	console.log(sc_wid, sc_hei);
+//	console.log(sc_wid, sc_hei);
 	sub.width = sc_wid;
 	sub.hei = sc_hei;
-	console.log(sub);
-	console.log(sub.width, sub.hei);
+//	console.log(sub);
+//	console.log(sub.width, sub.hei);
 }
 sub();
 
@@ -15,16 +15,6 @@ $(document).ready(function(){
 	// alert('hi');
 	
 	var numAc = $('.list').size(); // article 개수
-	// console.log(numAc);
-
-	// var widSec = 350*numAc;
-
-	// var widTotal = widSec+600; //큰사진 포함	
-
-	// $('.artists_list').width(widTotal);
-	// $("body").height(widSec);
-	// $("html,body").animate({"scrollTop":widSec},2000);
-	//맨 마지막 사진에 맞춰 스크롤이 맨아래로 가야하니까 설정해줌.
 
 	//스크롤 제어
 	$(window).on("scroll",function(){
@@ -34,23 +24,16 @@ $(document).ready(function(){
 
 	var $win_width = $(window).height;
 	$(".sub").css({'width' : $win_width});
-	//사진 제어
-	// $('article h2').on("click", function(e){
-	// 	e.preventDefault();
-
-	// 	var index = $(this).parent().index();
-	// 	var src = $(this).children('a').attr('href');
-	// 	var posAc = 193*index;
-
-	// 	$('article').removeClass();
-	// 	$(this).parent().addClass('on');
-
-	// 	$('article p img').attr({'src':''});
-	// 	$(this).siblings('p').children('img').attr({'src':src});
-	// 	$("html,body").scrollTop(posAc); //스크롤 제어
-
-	// 	$('article span').on("click", function(){
-	// 		$(this).parent().removeClass();
-	// 	});
-	// });
+    
+    
+    
+    //이미지클릭 기능
+    $(".list").on("click", function(){
+        $(".artists_view").fadeIn();
+        $(".close_btn").on("click", function(){
+            $(".artists_view").fadeOut();
+        });
+        
+    });
+    
 });
