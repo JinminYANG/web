60380236a4c5af1ddc2eeb41240f781c9266cec9
Jinmin <yangjinmin95@gmail.com>
2021-08-20 20:13:42 +0900

    Introduce Jinmin Web
     - Contact me
     - Footer
     - Scroll Sections Active Link

--- introduce_jinmin/assets/js/main.js
+++ introduce_jinmin/assets/js/main.js
@@ -114,6 +114,7 @@ let swiperPortfolio = new Swiper(".portfolio__container", {
 let swiperTestimonial = new Swiper(".testimonial__container", {
     loop: true,
     grabCursor: true,
+    spaceBetween: 48,
 
     navigation: {
         nextEl: ".swiper-button-next",
@@ -122,11 +123,34 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
