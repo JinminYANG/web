4760b481001dd55c94a08113cd081ba6745d6825
JinminZZang <yangjinmin95@gmail.com>
2022-05-10 19:18:11 +0900

    nft-baduk 랜딩페이지 제작중

---
+++ nft-baduk/js/index.js
@@ -0,0 +1,25 @@
+/*  nav click event */
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+       let top = window.scrollY;
+       let offset = sec.offsetTop;
+       let height = sec.offsetHeight;
+       let id = sec.getAttribute('id');
+
+       if(top >= offset && top < offset + height){
+           navLinks.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           });
+       }
+    });
+}
+
+/* iframe ratio calc */
+/*
+let videoPlayer = document.querySelector('#video-player');
+let width = videoPlayer.getAttribute('width');
+console.log(width);*/
