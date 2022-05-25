e57038bb6c94d15c3004f081fbf74ed12127aa42
JinminZZang <yangjinmin95@gmail.com>
2022-05-25 10:50:41 +0900

    nft sale open

--- nft-baduk/js/btnClick.js
+++ nft-baduk/js/btnClick.js
@@ -4,16 +4,11 @@ Array.from(buyBtns).forEach((buy) => {
     buy.addEventListener('click', (event) => {
         event.preventDefault();
 
-        alert('아직 오픈되지 않았습니다!');
-        // pageOpen();
+        // alert('아직 오픈되지 않았습니다!');
+        pageOpen();
     });
 });
 
 const pageOpen = () => {
-/*    const link = 'https://opensea.io/';
-    window.location.href = link;       //웹개발할때 숨쉬듯이 작성할 코드
-    // window.location.replace(link);     // 이전 페이지로 못돌아감
-    window.open(link);                 //window.open은 옵션이 여러가지 있음. 이렇게만하면 새창 뜸*/
-
-    window.open('https://opensea.io/');
+    window.open('https://opensea.io/collection/the-26th-lg-cup-world-baduk-championship-winner-sh');
 };
