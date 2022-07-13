a6c688a4744f0defd2137347c6ff0ec2da09e55a
JinminZZang <yangjinmin95@gmail.com>
2022-07-13 18:30:48 +0900

    asyaaf layout

--- asyaaf-test/js/workDetail.js
+++ asyaaf-test/js/workDetail.js
@@ -1,19 +1,61 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
+//* Form Validation
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation');
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
 
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
+        form.classList.add('was-validated');
+    }, false)
+});
 
-            form.classList.add('was-validated');
-        }, false)
-    })
-})();
+
+const countContainer = document.querySelector(".count-container");
+const minusBtn = countContainer.querySelector("#minus-btn");
+const plusBtn = countContainer.querySelector("#plus-btn");
+const counter = countContainer.querySelector(".counter");
+const amount = document.querySelector("#amount");
+const defaultAmount = document.querySelector("#amount").value;
+
+minusBtn.addEventListener("click", function () {
+    // console.log(counter.value);
+    counter.value--;
+    if (counter.value === '1') {
+        setDisabled(this);
+        amount.value = (defaultAmount * counter.value);
+    } else if (counter.value === '2') {
+        setAble(this);
+        setAble(plusBtn);
+        amount.value = (defaultAmount * counter.value);
+    } else {
+        setAble(this);
+        amount.value = (defaultAmount * counter.value);
+    }
+});
+
+plusBtn.addEventListener("click", function () {
+    // console.log(counter.value);
+    counter.value++;
+    if (counter.value === '1') {
+        amount.value = (defaultAmount * counter.value);
+    } else if (counter.value === '2') {
+        setAble(minusBtn);
+        amount.value = (defaultAmount * counter.value);
+    } else {
+        setDisabled(this);
+        amount.value = (defaultAmount * counter.value);
+    }
+});
+
+function setDisabled(item) {
+    item.setAttribute('disabled', 'disabled');
+}
+
+function setAble(item) {
+    item.removeAttribute('disabled');
+}
